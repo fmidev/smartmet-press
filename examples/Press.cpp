3fba535f05d1c45fe6d9f8638d9755ead0c69f4c
winberg <winberg>
2003-02-20 14:31:34 +0000

    ylin taso tarkistaa manageri-preprosessoinnin onnistumisen. Pääohjelma ei olisi muutenkaan selviytynyt.

--- examples/Press.cpp
+++ examples/Press.cpp
@@ -1,77 +1,144 @@
 // ======================================================================
+
 /*!
+
  * \file
+
  * \brief The main program for creating press products
+
  */
+
 // ======================================================================
+
 /*!
+
  * \page press Press
+
  *
+
  * Undocumented program.
+
  *
+
  */
+
 // ======================================================================
+
+
 
 #ifdef WIN32
+
  #pragma warning(disable : 4786) // poistaa n kpl VC++ k‰‰nt‰j‰n varoitusta
+
 #endif
 
+
+
 // press
+
 #include "NFmiPressManager.h"
+
 // newbase
+
 #include "NFmiCmdLine.h"   
-#include "NFmiYKJArea.h"
-#include "NFmiMetMath.h"
+
 #include "NFmiPreProcessor.h"
+
+
 
 using namespace std;
 
+
+
 int main(int argc, const char ** argv)
+
 {
 
+
+
   NFmiCmdLine cmdLine(argc,argv,"");
+
   NFmiString dataExt[4] = {"pre", "PRE", "prt", "PRT"};
+
   
+
   if(argc<=1)
+
     {
+
       cerr << "Error: Filename argument missing" << endl;
+
       return 1;
+
     }
+
   
+
   NFmiFileString descriptionFile(argv[1]);
+
   cout << "File: " << argv[1] << endl;
+
   
+
   bool ok;
+  bool managerReadFailed;
+
+
 
   if(descriptionFile.Extension() == dataExt[0] ||
+
 	 descriptionFile.Extension() == dataExt[1] ||
+
 	 descriptionFile.Extension() == dataExt[2] ||
+
 	 descriptionFile.Extension() == dataExt[3] 
+
 	 )
-	{
+
+   {
       NFmiPressProduct pressProduct;
-	  
+
       if(pressProduct.ReadDescriptionFile(NFmiString(descriptionFile))) 
-		{
+	  {
 		  NFmiPressManager manager;
-		  if(manager.ReadDescriptionAndWrite(pressProduct))
-			ok = true;
+		  if(manager.ReadDescriptionAndWrite(pressProduct, managerReadFailed))
+			  ok = true;
 		  else
-			{
-			  ok = pressProduct.WritePS();
-			}
-		}
+		  {
+			 if(managerReadFailed)
+				ok = false;
+			 else
+				ok = pressProduct.WritePS(); //manager not used
+
+		  }
+
+	  }
+
 	  else
-		{
+
+	  {
+
 		  ok = false;
-		}
+
+	  }
+
 	  pressProduct.Close();
+
 	  
+
 	  return (ok ? EXIT_SUCCESS : EXIT_FAILURE);
+
 	}
+
   cout << "ERROR: tuntematon tiedostotyyppi" << endl;
+
   
+
   return EXIT_FAILURE;
+
 }
 
+
+
 // ======================================================================
+
