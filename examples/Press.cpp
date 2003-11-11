b98b34563b73d0d2b229b881f866fbe4765e5fc9
mheiskan <mheiskan>
2003-11-11 08:17:19 +0000

    Output-hakemiston voi nyt vaihtaa -o optiolla, ja lokihakemiston -l optiolla.

--- examples/Press.cpp
+++ examples/Press.cpp
@@ -21,26 +21,59 @@
 // newbase
 #include "NFmiCmdLine.h"   
 #include "NFmiPreProcessor.h"
+#include "NFmiSettings.h"
 
 using namespace std;
 
 int main(int argc, const char ** argv)
 {
-
-  NFmiCmdLine cmdLine(argc,argv,"");
   NFmiString dataExt[4] = {"pre", "PRE", "prt", "PRT"};
+
+  NFmiCmdLine cmdLine(argc,argv,"o!l!");
+
+  if(cmdLine.Status().IsError())
+	{
+	  cerr << "Error on the command line:" << endl
+		   << " --> " << cmdLine.Status().ErrorLog() << endl;
+	  return 1;
+	}
   
-  if(argc<=1)
-    {
+  if(cmdLine.NumberofParameters() != 1)
+	{
       cerr << "Error: Filename argument missing" << endl;
       return 1;
     }
+
+  string logdir("lehtiLokiDir=");
+  if(cmdLine.isOption('l'))
+	{
+	  string dir = cmdLine.OptionValue('l');
+#ifdef UNIX
+	  NFmiSettings::Set("press::logpath",dir);
+#else
+	  logdir += dir;
+	  putenv(logdir.c_str());
+#endif	 
+	}
+
+  string outdir("lehtiOutDir=");
+  if(cmdLine.isOption('o'))
+	{
+	  string dir = cmdLine.OptionValue('o');
+#ifdef UNIX
+	  NFmiSettings::Set("press::outpath",dir);
+#else
+	  outdir += dir;
+	  putenv(outdir.c_str());
+#endif	 
+	}
+
   
-  NFmiFileString descriptionFile(argv[1]);
-  cout << "File: " << argv[1] << endl;
+  NFmiFileString descriptionFile(cmdLine.Parameter(1));
+  cout << "File: " << cmdLine.Parameter(1) << endl;
   
   bool ok;
- bool managerReadFailed;
+  bool managerReadFailed;
 
   if(descriptionFile.Extension() == dataExt[0] ||
 	 descriptionFile.Extension() == dataExt[1] ||
