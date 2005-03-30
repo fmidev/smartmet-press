0306abfc30de0cc73fe346395b09c84be9b27e6c
winberg <winberg>
2005-03-30 07:33:47 +0000

    AsemienMitta/Sijoitus-alue ei pelannut yhteen Kartan kanssa (BorgåBladet ym)

--- source/NFmiPressProduct.cpp
+++ source/NFmiPressProduct.cpp
@@ -1124,7 +1124,7 @@ bool NFmiPressProduct::ReadDescriptionFile(NFmiString inputFile)
  
    NFmiString writeString = inputFileName.Header();
    *itsLogFile << "** " << static_cast<char *>(writeString) << " **"<< endl;
-   *itsLogFile << "program version = 22.3.2005" << endl;       
+   *itsLogFile << "program version = 30.3.2005" << endl;       
    *itsLogFile << "Home dir " << static_cast<char *>(origHome) << ": " << static_cast<char *>(GetHome())  << endl;
 
    string inputStdName(origInputFileName);
