ca1e0e8b070562b822b3c3726912f2f767ec78f0
winberg <winberg>
2007-09-05 07:31:11 +0000

    + ValueCorrection

---
+++ include/NFmiValueCorrection.h
@@ -0,0 +1,122 @@
+// ======================================================================
+/*!
+ * \file
+ * \brief NFmiValueCorrection.h
+ */
+// ======================================================================
+/*!
+ * \class NFmiValueCorrection
+ *
+ *
+ * Tekee annetusta tiedostosta nimip‰iv‰taulukon. Tiedoston tulee olla muotoa:
+ *
+ * \code
+ *      ....
+ *		9.tammi	Veijo
+ *		9.tammi	Veikko
+ *		9.tammi	Veli
+ *		10.tammi	Nyyrikki
+ *		11.tammi	Kari
+ *		11.tammi	Karri
+ *      ....
+ * \endcode
+ *
+ * Oleellista on kaksi stringi‰ per rivi. Ekan tulee olla erilainen
+ * (sis‰llˆll‰ ei v‰li‰) jokaiselle uudelle p‰iv‰lle, ja tokassa tulee
+ * itse nimi. Samalle p‰iv‰lle voi antaa 1-n kappaletta
+ * nimi‰. Nimip‰iv‰ kysyt‰‰n oliolta MetTime:lla tuloksen ollessa
+ * muotoa "Kari, Karri". Tuloksen voi rajoittaa nimien lukum‰‰r‰n tai
+ * merkkim‰‰r‰n mukaan. Karkausp‰iv‰lle ei voi antaa arvoa. 
+ *
+ * Soveltuu myˆs muihin samankaltaisiin teht‰viin.
+ *
+ * K‰ytet‰‰n aluksi lehtis‰‰ohjelmasta.
+ *
+ */
+// ======================================================================
+
+#ifndef NFMIVALUECORRECTION_H
+#define NFMIVALUECORRECTION_H
+
+#include "NFmiMetTime.h"
+#include "NFmiString.h"
+#include <vector>
+
+struct FmiTempCorrections
+{
+	NFmiString stationName;
+	float dayCorr;
+	float nightCorr;
+};
+//! Undocumented
+class _FMI_DLL NFmiValueCorrection 
+{
+
+public:
+
+  NFmiValueCorrection(void);
+  virtual ~NFmiValueCorrection(void);
+  void SetFiles(const std::string & theFile, std::ofstream * theLogFile);
+  bool IsRead(void);
+  bool IsMissing(void);
+  bool CorrectValue(float& thevalue, const NFmiString& theStation,
+								bool dayTime);
+private:
+    //bool ReadFile(const NFmiString & theFileName);  
+  bool ReadFile(void);  
+
+private:
+  std::string itsFile;
+  std::ofstream* itsLogFile;       
+  std::vector<FmiTempCorrections> itsTempCorrections;
+  bool fRead;
+  bool fMissing;
+
+}; // class NFmiValueCorrection
+
+
+
+// ----------------------------------------------------------------------
+/*!
+ * Void constructor
+ */
+// ----------------------------------------------------------------------
+
+inline
+NFmiValueCorrection::NFmiValueCorrection(void)
+{
+  fRead = false;
+  fMissing = false;
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * Undocumented
+ *
+ * \return Undocumented
+ */
+// ----------------------------------------------------------------------
+
+inline
+bool NFmiValueCorrection::IsRead(void)
+{
+  return fRead;
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * Undocumented
+ *
+ * \return Undocumented
+ */
+// ----------------------------------------------------------------------
+
+inline
+bool NFmiValueCorrection::IsMissing(void)
+{
+return fMissing;
+}
+
+#endif // NFMIVALUECORRECTION_H
+
+// ======================================================================
---
+++ source/NFmiValueCorrection.cpp
@@ -0,0 +1,140 @@
+// ======================================================================
+/*
+ * \file
+ * \brief Implementation of class NFmiValueCorrection
+ */
+// ======================================================================
+
+#include "NFmiValueCorrection.h"
+#include <iostream>
+//#include <sstream>
+
+using namespace std;
+
+// ----------------------------------------------------------------------
+/*!
+ */
+// ----------------------------------------------------------------------
+
+void NFmiValueCorrection::SetFiles(const string & theFile, ofstream* theLogFile)
+{
+  itsFile = theFile;
+  itsLogFile = theLogFile;
+}
+// ----------------------------------------------------------------------
+/*!
+ * The destructor does nothing special
+ */
+// ----------------------------------------------------------------------
+
+NFmiValueCorrection::~NFmiValueCorrection(void)
+{
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * Undocumented
+ *
+ * \return Undocumented
+ */
+// ----------------------------------------------------------------------
+
+bool NFmiValueCorrection::ReadFile(void)
+{  
+  fRead = true;
+  //fValue = false;
+  string str1, str2, str3;
+  
+  //string includePath(itsFile);
+  //ifstream in(includePath.c_str(),ios::in|ios::binary);
+  ifstream in(itsFile.c_str(),ios::in|ios::binary);
+
+  if(!in)
+	{
+		fMissing = true;
+		*itsLogFile << "*** WARNING: ei Lampokorjaus-tiedostoa" << endl;
+		return false;
+	}
+
+  string::size_type start1, end1, start2, end2, start3, end3;
+  const string delims(" \t");
+  string line;
+  while(getline(in,line))
+	{
+	  // Ignore the line if it is a comment line or too short
+
+	  line += " "; //rivin lopetusmerkki
+	  if(line.substr(0,3)=="END" || line.substr(0,5)=="LOPPU")
+		break;
+
+	  if(line[0]=='#' || line.substr(0,2)=="//" || line.size() < 6)
+		continue;
+
+	  start1 = line.find_first_not_of(delims);
+	  end1 = line.find_first_of(delims, start1);
+	  start2 = line.find_first_not_of(delims, end1);
+	  end2 = line.find_first_of(delims, start2);
+	  start3 = line.find_first_not_of(delims, end2);
+	  end3 = line.find_first_of(delims, start3); 
+	  //end3 = line.find_last_not_of(delims, start3); //HUOM
+
+	  if(end3 == string::npos)
+		{
+		  *itsLogFile << "***ERROR: invalid line in Lampokorjaus: "
+					  << line
+					  << endl;
+		  continue;
+		}
+	  else
+		{
+		  FmiTempCorrections tempCorrection;
+		  str1 = line.substr(start1, end1-start1);
+		  str2 = line.substr(start2, end2-start2);
+		  str3 = line.substr(start3, end3-start3);
+		  tempCorrection.stationName = str1;
+          tempCorrection.dayCorr   = static_cast<float>(atof(str2.c_str()));
+          tempCorrection.nightCorr = static_cast<float>(atof(str3.c_str()));
+		  itsTempCorrections.push_back(tempCorrection);
+		}
+	}
+	*itsLogFile << "   Lampokorjaus-tiedosto luettu" << endl;
+	in.close();
+	in.clear();
+	return true;
+}
+
+// ----------------------------------------------------------------------
+/*!
+ * Undocumented
+ *
+ * \return Undocumented
+ */
+// ----------------------------------------------------------------------
+
+bool NFmiValueCorrection::CorrectValue(float& thevalue, const NFmiString& theStation,
+								bool dayTime)
+{
+	if(!IsRead())
+		ReadFile();
+
+	if(IsMissing())
+		return false;
+
+	std::vector<FmiTempCorrections>::iterator pos;
+
+	for(pos=itsTempCorrections.begin(); pos != itsTempCorrections.end(); ++pos)
+	{
+		if((*pos).stationName == theStation)
+		{
+			if(dayTime)
+				thevalue += (*pos).dayCorr;
+			else
+				thevalue += (*pos).nightCorr;
+
+			return true;
+		}
+	}
+	return false;
+}
+
+// ======================================================================
