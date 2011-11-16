9f0f03ee97ac5f34ab00c375d5268697c0fc1e7a
Lars Winberg <lars.winberg@fmi.fi>
2011-11-16 13:59:16 +0200

    "n stations*times processed" ilmoitus pois kun aluekäsittely (esim max)

--- source/NFmiPressProduct.cpp
+++ source/NFmiPressProduct.cpp
@@ -1459,7 +1459,7 @@ bool NFmiPressProduct::ReadDescriptionFile(NFmiString inputFile)
  
    NFmiString writeString = inputFileName.Header();	
    *itsLogFile << "** " << static_cast<char *>(writeString) << " **"<< endl;
-   *itsLogFile << "program version = Release 11.11.11" << endl;       
+   *itsLogFile << "program version = Release 16.11.11" << endl;       
    *itsLogFile << "Home dir " << static_cast<char *>(origHome) << ": " << static_cast<char *>(GetHome())  << endl;
 
    string inputStdName(origInputFileName);
